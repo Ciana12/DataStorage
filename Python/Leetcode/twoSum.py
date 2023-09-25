1a7014a06a835b189bd1339a02f9af25773370f6
kyoxi_de <90842745+kyoxi@users.noreply.github.com>
2023-09-25 19:08:18 +0700

    1. two sum
    python solution for (1. two sum) in leetcode. Is there anyways to make it faster just push your code up :D

---
+++ Python/Leetcode/twoSum.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        num_indices = {}  # Dictionary to store the indices of numbers
+        
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_indices:
+                return [num_indices[complement], i]
+            num_indices[num] = i
+        
+        return []  # If no solution is found
