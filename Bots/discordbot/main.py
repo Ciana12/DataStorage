80a8d44798862e577cd5721d57105150d8358fb7
kyoxi <yamadamisaki1102@gmail.com>
2023-10-02 18:38:06 +0700

    updated ping command :D

--- Bots/discordbot/main.py
+++ Bots/discordbot/main.py
@@ -6,12 +6,27 @@ logger = settings.logging.getLogger("bot")
 def run():
 
     intents = discord.Intents.default()
+    intents.message_content = True
 
     bot = commands.Bot(command_prefix = "-", intents = intents)
 
     @bot.event
     async def on_ready():
         logger.info(f"User: {bot.user} (ID: {bot.user.id})")
+
+    @bot.command(
+        aliases = ['p'],
+        
+        help = 'this is help',
+        description = 'this is description',
+        brief = 'this is brief',
+
+        enable = True,
+        hidden = False
+    )
+    async def ping(ctx):
+        """ ping pong  """
+        await ctx.send("pong")
 
 
     bot.run(settings.DISCORD_API_SECRET, root_logger = True)
