114e20c68c1a5cb13f490fa0bc1f91086ef600e0
kyoxi <yamadamisaki1102@gmail.com>
2023-10-03 19:41:00 +0700

    updated simple math commands such as ( add, substract, div, mult )

--- Bots/discordbot/main.py
+++ Bots/discordbot/main.py
@@ -1,3 +1,4 @@
+import random
 import settings
 import discord
 from discord.ext import commands
@@ -13,13 +14,25 @@ def run():
     @bot.event
     async def on_ready():
         logger.info(f"User: {bot.user} (ID: {bot.user.id})")
+    
+    async def on_command_error(ctx, error):
+        if isinstance(error, MissingRequiredArgument):
+            await ctx.send("handled error globally")
+
+
+    # @add.error
+    # async def add_error(ctx, error):
+    #     if isinstance(error, commands.MissingRequiredArgument):
+    #         await ctx.send("handled error locally")
+
+
 
     @bot.command(
         aliases = ['p'],
-        
-        help = 'this is help',
-        description = 'this is description',
-        brief = 'this is brief',
+
+        # help = 'this is help',
+        # description = 'this is description',
+        # # brief = 'this is brief',
 
         enable = True,
         hidden = False
@@ -27,6 +40,38 @@ def run():
     async def ping(ctx):
         """ ping pong  """
         await ctx.send("pong")
+
+    
+
+    @bot.group()
+    async def math(ctx):
+        if ctx.invoked_subcommand is None:
+            await ctx.send(f"No, {ctx.subcommand_passed} does not belong to math")
+
+
+    @math.command()
+    async def add(ctx, one : int, two : int):
+        await ctx.send(one + two)
+
+
+    @math.command()
+    async def substract(ctx, one : int, two : int):
+        await ctx.send(one - two)
+
+
+    @math.command()
+    async def divide(ctx, one: int , two: int):
+        await ctx.send(one / two)
+    
+
+    @math.command()
+    async def multiple(ctx, one: int, two: int):
+        await ctx.send(one * two)
+
+    
+
+
+        
 
 
     bot.run(settings.DISCORD_API_SECRET, root_logger = True)
